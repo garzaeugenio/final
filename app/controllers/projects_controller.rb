3c469df36692b5c4266b74d93ae8ef18c99c96a1
Eugenio Garza <egarza2015@kellogg.northwestern.edu>
2015-02-13 01:05:25 -0600

    Week5
    Week5

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,11 +6,35 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find_by(id: params["id"])
-    @owners = Owner.where(project_id: @project.id)
-    @users = []
-    @owners.each do |owner|
-      the_user = User.find_by(id: owner.user_id)
-      @users << the_user
-      end 
+    @owner = Owner.where(project_id: @project.id)
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project_params = params.require(:project).permit(:project_name)
+    Project.create(project_params)
+    render text: params["project"]
+  end
+
+  def edit
+    @project = Project.find_by(id: params["id"])
+
+  end
+
+  def update
+    project_params = params.require(:project).permit(:project_name)
+    @project = Project.find_by(id: params["id"])
+    @project.update(project_params)
+    redirect_to project_path
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :owner
 end
